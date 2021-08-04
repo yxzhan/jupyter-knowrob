4841fa3bf4522bfee04b2427b2cd9ed8e4456446
Sascha Jongebloed <sasjonge@uni-bremen.de>
2021-08-04 17:11:11 +0200

    First working dockerization

---
+++ docker/run_notebook.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+source /home/ros/devel/setup.bash
+jupyter lab --ip=0.0.0.0 --allow-root
